8d150d5a3964b77e5497b33286872d6b2a5dce4f
Rene B. Dena <renedena842@gmail.com>
2021-10-17 20:01:02 -0700

    API Implementation

--- bin/renice/Renice.cpp
+++ bin/renice/Renice.cpp
@@ -3,35 +3,62 @@
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
-#include <Process.h>
-#include <ProcessManager.h>
-//#include <sys/Renice.h>
+
+// Renice header(C:/FreeNOS-1.0.3/bin/renice/Renice.h)
 #include "Renice.h"
+
+// Libary implementation
+// Renice system header(C:/FreeNOS-1.0.3/lib/libposix/sys/renice/Renice.h)
+#include "sys/renice.h"
+// Process client header(C:/FreeNOS-1.0.3/lib/libruntime/ProcessClient.h)
+#include <ProcessClient.h>
 
 Renice::Renice(int argc, char** argv) : POSIXApplication(argc, argv)
 {
 	parser().setDescription("Renice for a state change within the process");
-	parser().registerPositional("PROCESS_ID", "Renice for the given process until it returns or finishes");
+    parser().registerPositional("Priority", "The current set priority of the process");
+	parser().registerPositional("PID", "The Process ID of the named process");
+	parser().registerFlag('n', "priority", "Parameter that sets a new priority to the process");
+    newPriority = atoi(argv[2]);
+    processID = atoi(argv[3]);
 }
 
-Renice::~Renice() {
+Renice::~Renice()
+{
 	// Nothing
 }
 
 // Execution method, performs system call on renicePID using user-provided Process ID.
-Renice::Result Renice::exec() {
-	ProcessID arg_id;
-	int store_result, success;
-	// Check if user enters reserved
-	if ((arg_id = atoi(arguments().get("PROCESS_ID"))) <= 3) {
-		ERROR("Invalid PID `" << arguments().get("PROCESS_ID") << "'");
-		return InvalidArgument;
-	}
+Renice::Result Renice::exec()
+{
+    printf("processID = %d\n", processID);
+    printf("newPriority = %d\n", newPriority);
+    bool result;
+    ProcessClient process;
 
-	//success = renicepid(arg_id, &store_result, 0);
-	if (!success) {
-		ERROR("Could not renice for " << arguments().get("PROCESS_ID") << "'");
-		return TimedOut;
-	}
-	return Success;
+    // If priority is set less than 1 or greater than 5 (invalid)
+    if (newPriority < 1 || newPriority > 5)
+	{
+        result = false;
+        printf("Invalid new set priority.\n");
+    }
+	// If check is false (valid)
+    else
+	{
+        process.setPriority(processID, newPriority);
+        result = true;
+        printf("Validated Priority.\n");
+    }
+    
+    // Print result prompt after check
+    if (result == true)
+	{
+        printf("Priority has been changed.\n");
+    }
+    else
+	{
+        printf("Priority has NOT been changed.\n");
+    }
+
+    return Success;
 }
